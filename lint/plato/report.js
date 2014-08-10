5eb9f3fcf83cb56b743e1e31918b394c2c6ba5fe
Legokichi Duckscallion <legokichi@duxca.com>
2014-08-10 20:46:15 +0900

    fix global name

---
+++ lint/plato/report.js
@@ -0,0 +1,63 @@
+__report = {
+  "reports": [
+    {
+      "info": {
+        "file": "lib/WebModule.JSZip.js",
+        "fileShort": "lib/WebModule.JSZip.js",
+        "fileSafe": "lib_WebModule_JSZip_js",
+        "link": "files/lib_WebModule_JSZip_js/index.html"
+      },
+      "jshint": {
+        "messages": 95
+      },
+      "complexity": {
+        "aggregate": {
+          "line": 1,
+          "complexity": {
+            "sloc": {
+              "physical": 8997,
+              "logical": 3705
+            },
+            "cyclomatic": 740,
+            "halstead": {
+              "operators": {
+                "distinct": 64,
+                "total": 10008,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "operands": {
+                "distinct": 1354,
+                "total": 12835,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "length": 22843,
+              "vocabulary": 1418,
+              "difficulty": 303.3382570162482,
+              "volume": 239158.02803120227,
+              "effort": 72545779.37442791,
+              "bugs": 79.71934267706742,
+              "time": 4030321.076357106
+            },
+            "params": 481
+          }
+        },
+        "module": "lib/WebModule.JSZip.js",
+        "maintainability": 56.3358094779206
+      }
+    }
+  ],
+  "summary": {
+    "total": {
+      "sloc": 8997,
+      "maintainability": 56.3358094779206
+    },
+    "average": {
+      "sloc": 8997,
+      "maintainability": "56.34"
+    }
+  }
+}
